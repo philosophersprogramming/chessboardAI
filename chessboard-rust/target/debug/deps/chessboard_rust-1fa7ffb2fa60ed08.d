7f4dbf7565ca1638c58c15d7e89939629ae19b96
Akash Kallumkal <103540537+Xocash695@users.noreply.github.com>
2024-01-16 14:31:27 -0500

    Delete chessboard-rust/target/debug directory

--- chessboard-rust/target/debug/deps/chessboard_rust-1fa7ffb2fa60ed08.d
+++
@@ -1,5 +0,0 @@
-/Users/akash/Source/chessAI/Chessboard_Recognition/chessboard-rust/target/debug/deps/chessboard_rust-1fa7ffb2fa60ed08.rmeta: src/main.rs
-
-/Users/akash/Source/chessAI/Chessboard_Recognition/chessboard-rust/target/debug/deps/chessboard_rust-1fa7ffb2fa60ed08.d: src/main.rs
-
-src/main.rs:
--- chessboard-rust/target/debug/deps/chessboard_rust-a47ba0d849c217ca.d
+++
@@ -1,5 +0,0 @@
-/Users/akash/Source/chessAI/Chessboard_Recognition/chessboard-rust/target/debug/deps/chessboard_rust-a47ba0d849c217ca.rmeta: src/main.rs
-
-/Users/akash/Source/chessAI/Chessboard_Recognition/chessboard-rust/target/debug/deps/chessboard_rust-a47ba0d849c217ca.d: src/main.rs
-
-src/main.rs:

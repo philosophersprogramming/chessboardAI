e6b068fc5f99807419d3d367e1d1892c052ab346
Xocash695 <akash.kallumkal@gmail.com>
2024-01-17 19:59:43 -0500

    added save to detected_image for debuging

--- final_algorithm/start.py
+++ final_algorithm/start.py
@@ -3,12 +3,12 @@ from crop import *
 from boxout import *
 from pieces import *
 start = time.time()
-crop("../images/board7.jpg")
+crop("../images/board9.jpg")
 image_path = 'cropped.png'
 rows = 8  # Adjust this value based on your chessboard
 cols = 8  # Adjust this value based on your chessboard
 output_folder = 'out/'  # Change this to your desired output folder
 find_chessboard_intersections(image_path, rows, cols, output_folder)
 end = time.time()
-pieces("out/")
+#pieces("out/")
 print("It took " + str(end - start) + " to finish. " + "The output saved in " + output_folder)

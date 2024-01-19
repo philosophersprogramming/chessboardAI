4c5c3ce98ce8512a53a6ef7e4b832d32e73e033e
nathankim <nathank0707@gmail.com>
2024-01-19 11:23:32 -0500

    i edge

--- piece_location/compute_position.py
+++ piece_location/compute_position.py
@@ -7,31 +7,70 @@ class BoardComparator:
         # this creates an empty 8x8 array for the chessboard
         self.fen = fen
         self.board = [[0 for _ in range(8)] for _ in range(8)]
+        # 0 - empty square
+        # 1 - white piece
+        # 2 - black piece
         self.turn = turn
         
         position_fen = fen.split(" ")[0].split("/")
-        for row in range(len(position_fen)):
-            for col in range(len(position_fen[row])):
-                if position_fen[row][col].isnumeric():
-                    pass
+        for row in range(8):
+            col = 0
+            for piece in position_fen[row]:
+                if piece.isnumeric():
+                    for i in range(int(piece)):
+                        self.board[row][col + i] = ''
+                    col += int(piece)
                 else:
-                    if position_fen[row][col].isupper():
-                        self.board[row][col] = 1
-                    else:
-                        self.board[row][col] = 2
-        
+                    self.board[row][col] = piece
+                    col += 1
+                
     def make_move(self, newboard):
         for row in range(8):
             for col in range(8):
-                if newboard[row][col] != self.board[row][col]:
-                    if newboard[row][col] == 
-                        from_tile = (row, col)
+                if self.board[row][col] == "":
+                    convert_tile = 0
+                elif self.board[row][col].isupper():
+                    convert_tile = 1
+                else:
+                    convert_tile = 2
+                    
+                if newboard[row][col] != convert_tile:
+                    if newboard[row][col] == self.turn:
+                        to_tile = (row, col)
                     else:
-                        
-                
-    def get_fenstring():
-        pass
+                        from_tile = (row, col)
+        self.board[to_tile[0]][to_tile[1]] = self.board[from_tile[0]][from_tile[1]]
+        self.board[from_tile[0]][from_tile[1]] = ""
+        print(self.board)
+                    
+    def get_fenstring(self):
+        fen = ""
+        for row in self.board:
+            empty_counter = 0
+            for piece in row:
+                if piece == '':
+                    empty_counter += 1
+                else:
+                    if empty_counter != 0:
+                        fen += str(empty_counter)
+                        empty_counter = 0
+                    fen += piece
+            if empty_counter != 0:
+                fen += str(empty_counter)
+            fen += "/"
+        return fen
 
 if __name__ == "__main__":
-    comparator = BoardComparator("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    
+    comparator = BoardComparator("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 1)
+    print(comparator.get_fenstring())
+    newboard = [
+        [2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1]
+    ]
+    comparator.make_move(newboard)

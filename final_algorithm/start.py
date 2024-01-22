090009d4a35a4f31f5664351d72e1d52ddd4ca3d
Xocash695 <akash.kallumkal@gmail.com>
2024-01-22 07:50:47 -0500

    added boards and added time to pieces

--- final_algorithm/start.py
+++ final_algorithm/start.py
@@ -3,12 +3,12 @@ from crop import *
 from boxout import *
 from pieces import *
 start = time.time()
-crop("../images/board3.jpg")
+crop("../images/board18.jpg")
 image_path = 'cropped.png'
 rows = 8  # Adjust this value based on your chessboard
 cols = 8  # Adjust this value based on your chessboard
 output_folder = 'out/'  # Change this to your desired output folder
 find_chessboard_intersections(image_path, rows, cols, output_folder)
-end = time.time()
 pieces("out/")
+end = time.time()
 print("It took " + str(end - start) + " to finish. " + "The output saved in " + output_folder)

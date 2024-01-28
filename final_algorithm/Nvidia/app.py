4e2fb2373b14a467bd6cb6b0aec9c5d7ac18bc5d
Xocash695 <akash.kallumkal@gmail.com>
2024-01-28 04:37:58 +0000

    added Receiving moves

--- final_algorithm/Nvidia/app.py
+++ final_algorithm/Nvidia/app.py
@@ -2,10 +2,20 @@ from flask import Flask, request, jsonify
 from PIL import Image
 from start import *
 import tempfile
+from received import *
 import os
 
 
 app = Flask(__name__)
+
+def is_valid_move(move):
+    # Add your custom logic to validate a chess move
+    # For simplicity, checking if the move is two characters
+    return len(move) == 2
+
+def extract_moves(data):
+    moves = [data[i:i+2] for i in range(0, len(data), 2)]
+    return moves if all(is_valid_move(move) for move in moves) else None
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
@@ -33,6 +43,25 @@ def upload_image():
 
     output = start(temp_filename)
     return jsonify(output)
+@app.route('/chessmove', methods=['POST'])
+def chess_move():
+    if request.method == 'POST':
+        data = request.form.get('move')
+
+        if data is None or data.strip() == '':
+            return "Chess move data is empty"
+
+        extracted_moves = extract_moves(data)
+
+        if extracted_moves is None or len(extracted_moves) != 2:
+            return "Invalid chess move data. Please provide exactly two valid moves."
+
+        move1, move2 = extracted_moves
+        received(move1+move2)
+        
+        return f"Received chess moves: {move1}, {move2}"
+    else:
+        return "Invalid request method"
 
 if __name__ == '__main__':
     initial_array_path = 'initial_array.pkl'
--- final_algorithm/Nvidia/received.py
+++ final_algorithm/Nvidia/received.py
@@ -1,5 +1,25 @@
 import pickle
-with open('initial_array.pkl', 'rb') as initial_file:
-    initial_array = pickle.load(initial_file)
-for row in initial_array:
-    print(row)
+def received(move):
+    with open('initial_array.pkl', 'rb') as initial_file:
+        initial_array = pickle.load(initial_file)
+    def make_move(chessboard, move):
+        from_row, from_col, to_row, to_col = 8 - int(move[1]) , ord(move[0]) - ord('a'), 8 - int(move[3]) , ord(move[2]) - ord('a')
+
+        if 0 <= from_row < 8 and 0 <= from_col < 8 and 0 <= to_row < 8 and 0 <= to_col < 8:
+            piece_value = chessboard[from_row][from_col]
+            chessboard[to_row][to_col] = piece_value
+            chessboard[from_row][from_col] = 0
+        else:
+            print("Invalid move. Out of bounds.")
+
+    # Example usage:
+
+
+    make_move(initial_array, move)  # Corrected move to "a7a5"
+
+    print("\nAfter move:")
+    for row in initial_array:
+        print(row)
+
+    with open('initial_array.pkl', 'wb') as initial_file:
+        pickle.dump(initial_array, initial_file)

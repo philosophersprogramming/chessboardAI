ab6ff99f945cd44ff1fbe7067d974840afed6066
Xocash695 <akash.kallumkal@gmail.com>
2024-02-15 16:31:45 -0500

    cleanup

--- Finding_pieces/AI_detection/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8n-cls.pt')
-model.train(data="LOCATION TO MODEL", epochs=10, batch=-1, imgsz=640)
-metrics = model.val()
-metrics.top1 # top1 accuracy
-metrics.top5 # top5 accuracy
